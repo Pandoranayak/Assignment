664d73e9a2ebcc0e5537755cdf039b8777fc6f82
Pandoranayak <166613656+Pandoranayak@users.noreply.github.com>
2025-04-07 23:06:06 +0530

    Update menu_item_validation_step.py

--- steps/menu_item_validation_step.py
+++ steps/menu_item_validation_step.py
@@ -1,19 +1,19 @@
-from behave import Given, Then, When
+from behave import given, then, when
 from playwright.sync_api import expect
 
-@Given('the user is on page "{url}"')
+@given('the user is on page "{url}"')
 def step_impl(context, url):
     context.logger.info(f"Navigating to: {url}")
     context.page.goto(url, wait_until="networkidle")
     context.logger.info("Navigation completed.")
 
-@When('the user clicks on categories')
+@when('the user clicks on categories')
 def click_categories(context):
     context.logger.info("Clicking on categories...")
     context.page.locator('[data-test=\"nav-categories\"]').click()
     context.logger.info("Clicked on categories.")
 
-@Then('the dropdown should be displayed with all items in order')
+@then('the dropdown should be displayed with all items in order')
 def check_dropdown_items(context):
     context.logger.info("Checking if the dropdown is displayed...")
     dropdown = context.page.locator("ul[aria-label='nav-categories'] ")
@@ -33,20 +33,20 @@ def check_dropdown_items(context):
         expect(dropdown.locator(f"text={item}")).to_be_visible()
         context.logger.info(f"Item '{item}' is visible in the dropdown.")
 
-@When('user click on the category "Rentals"')
+@when('user click on the category "Rentals"')
 def click_category(context):
     context.logger.info(f"Clicking on category: Rentals")
     context.page.locator("[data-test='nav-rentals']").click()
     context.logger.info(f"Clicked Rental category")
 
 
-@Then('the user is taken to the landing page for "Rentals"')
+@then('the user is taken to the landing page for "Rentals"')
 def check_landing_page(context,):
     context.logger.info(f"Checking landing page for category: Rentals")
     expect(context.page).to_have_url("https://practicesoftwaretesting.com/rentals")
     context.logger.info("Landing page URL is correct.")
 
-@Then('the title of the landing page should be "Rentals Overview - Practice Software Testing - Toolshop - v5.0"')
+@then('the title of the landing page should be "Rentals Overview - Practice Software Testing - Toolshop - v5.0"')
 async def check_landing_page_title(context):
     context.logger.info(f"Checking landing page title.")
     act_title = await context.page.title()
